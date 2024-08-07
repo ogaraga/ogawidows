64d60ad2f8ede510dc9ab600b1b851838a9c5cb7
Ogaraga <raymond.ogaraga@gmail.com>
2024-08-08 00:19:40 +0100

    update

---
+++ sever.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('CONTACT'))
+//get the form
+app.get('/',(req, res)=>{
+    res.sendFile(__dirname + '/CONTACT/cont.html')
+});
+//post the form
+app.post('/', (req, res)=>{
+    const myName = req.body.myName;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const time = req.body.time;
+    const statuz = req.body.statuz;
+    const mycomment = req.body.mycomment;
+    const male = req.body.male;
+    const female = req.body.female;
+    const country = req.body.country;
+    const address = req.body.address;
+    
+    //send email to inbox
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user:'raymond.ogaraga@gmail.com',
+            pass:'qxfvfqwklfieagvf'
+        }
+    })
+    const mailOptions = {
+        from: email,
+        to: 'raymond.ogaraga@gmail.com',
+        subject: `A message from ${statuz}. ${myName} at ${time}.`,
+        text: `${mycomment}. These are my details viz: \n Phone no: ${phone}\n Address: ${address}\n Email: ${email}\n Country:${country}\n Gender: Male/${male},Female/${female} `
+    }
+    transporter.sendMail(mailOptions, (err, info)=>{
+        if(err){
+            console.log('error has occurred!')
+        }else{
+            console.log('Mail was sent: '+ info.response)
+        }
+    })
+});
+app.listen(5500, ()=>console.log('Server running @ port 5500!'));
