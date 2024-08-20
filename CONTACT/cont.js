adb2a021d2bde58351bf610c319ecf1e31ac1ea6
Ogaraga <raymond.ogaraga@gmail.com>
2024-08-20 14:39:05 +0100

    updated

--- CONTACT/cont.js
+++ CONTACT/cont.js
@@ -1,59 +1,59 @@
-const container = document.getElementById("form__container");
-const mySubmit = document.querySelector(".submited");
-const myName = document.getElementById("fullname");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const time = document.getElementById("time");
-const male = document.getElementById("male");
-const female = document.getElementById("female");
-const address = document.getElementById("address");
-const mycomment = document.getElementById("messages");
-const country = document.getElementById("selected");
-const statuz = document.getElementById("prefix");
-const myForm = document.getElementById("form__wrapper");
-mySubmit.addEventListener("submit", (e) => {
-    e.preventDefault();
-    try {
-        const formData = {
-            statuz: statuz.options[statuz.selectedIndex].text,
-            country: country.options[country.selectedIndex].text,
-            mycomment: mycomment.value,
-            address: address.value,
-            female: female.checked,
-            male: male.checked,
-            myName: myName.value,
-            time: time.value,
-            email: email.value,
-            phone: phone.value,
-        };
+// const container = document.getElementById("form__container");
+// const mySubmit = document.querySelector(".submited");
+// const myName = document.getElementById("fullname");
+// const email = document.getElementById("email");
+// const phone = document.getElementById("phone");
+// const time = document.getElementById("time");
+// const male = document.getElementById("male");
+// const female = document.getElementById("female");
+// const address = document.getElementById("address");
+// const mycomment = document.getElementById("messages");
+// const country = document.getElementById("selected");
+// const statuz = document.getElementById("prefix");
+// const myForm = document.getElementById("form__wrapper");
+// mySubmit.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     try {
+//         const formData = {
+//             statuz: statuz.options[statuz.selectedIndex].text,
+//             country: country.options[country.selectedIndex].text,
+//             mycomment: mycomment.value,
+//             address: address.value,
+//             female: female.checked,
+//             male: male.checked,
+//             myName: myName.value,
+//             time: time.value,
+//             email: email.value,
+//             phone: phone.value,
+//         };
 
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "/");
-        xhr.setRequestHeader("content-type", "application/json");
-        xhr.onload = () => {
-            console.log(xhr.status);
-            if (xhr.status === 200 && [male.checked || female.checked]) {
-                myForm.style.display = "none";
-                container.innerHTML =
-                    "Your submission is well received. Thank you!";
-                container.style.background = "white";
-                container.style.width = "50%";
-                container.style.padding = "20px";
-                container.style.color = "green";
-                xhr.send(JSON.stringify(formData));
-            } else {
-                setTimeout(() => {
-                    myForm.innerHTML = "oops! please, fill in all necessary blocks";
-                    myForm.style.color = "red";
-                    myForm.style.background = "white";
-                    myForm.style.width = "100%";
-                }, 1000);
-                myForm.style.display = "block";
+//         const xhr = new XMLHttpRequest();
+//         xhr.open("POST", "/send_mail");
+//         xhr.setRequestHeader("content-type", "application/json");
+//         xhr.onload = () => {
+//             console.log(xhr.status);
+//             if (xhr.status === 200 && [male.checked || female.checked]) {
+//                 myForm.style.display = "none";
+//                 container.innerHTML =
+//                     "Your submission is well received. Thank you!";
+//                 container.style.background = "white";
+//                 container.style.width = "50%";
+//                 container.style.padding = "20px";
+//                 container.style.color = "green";
+//                 xhr.send(JSON.stringify(formData));
+//             } else {
+//                 setTimeout(() => {
+//                     myForm.innerHTML = "oops! please, fill in all necessary blocks";
+//                     myForm.style.color = "red";
+//                     myForm.style.background = "white";
+//                     myForm.style.width = "100%";
+//                 }, 1000);
+//                 myForm.style.display = "block";
 
-            }
-        }
+//             }
+//         }
 
-    } catch (error) {
-        console.log('Something went wrong!')
-    }
-});
+//     } catch (error) {
+//         console.log('Something went wrong!')
+//     }
+// });
--- sever.js
+++ sever.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 app.use(express.static('CONTACT'))
 //get the form
 app.get('/',(req, res)=>{
     res.sendFile(__dirname + '/CONTACT/cont.html')
 });
 //post the form
-app.post('/', (req, res)=>{
-    try {        
+app.post('/send_mail', (req, res)=>{
+         
     const myName = req.body.myName;
     const email = req.body.email;
     const phone = req.body.phone;
     const time = req.body.time;
     const statuz = req.body.statuz;
-    const mycomment = req.body.mycomment;
+    const mycomment = req.body.message;
     const male = req.body.male;
     const female = req.body.female;
     const country = req.body.country;
@@ -41,15 +43,15 @@ app.post('/', (req, res)=>{
         text: `${mycomment}. These are my details viz: \n Phone no: ${phone}\n Address: ${address}\n Email: ${email}\n Country:${country}\n Gender: Male/${male},Female/${female} `
     }
     transporter.sendMail(mailOptions, (err, info)=>{
-        if(err || (male === undefined && female === undefined)){
-            console.log('error has occurred!')
+        if(err){
+            console.log('Error has occurred sending the message')
         }else{
-            console.log('Mail was sent: '+ info.response)
+            res.sendFile(__dirname + '/CONTACT/cont2.html')
         }
-    });
+        
+    console.log(info)
+    })
+       
     
-    } catch (error) {
-        console.log(error.message)
-    }
-});
+    });
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}!`));
